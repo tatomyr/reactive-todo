2264857686289ee65f697c6e586edbc7c9c01bba
tatomyr <andrew.tatomyr@gmail.com>
2018-04-06 01:38:16 +0300

    remove old files

--- src/Counter/index.js
+++
@@ -1,8 +0,0 @@
-import Inc from './Inc.js'
-import Dec from './Dec.js'
-
-// A tiny counter
-export default () => `
-  ${Inc()}
-  ${Dec()}
-`
--- src/Test/Rnd.js
+++
@@ -1,4 +0,0 @@
-import { render, renderTextField, mutate } from '../store-provider'
-
-const Rnd = ({ test }) => `<span>${test}</span>`
-export default render(rnd, 'test')
