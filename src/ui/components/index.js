3559d390645902e71e5cc7aa732d7c95a2eaf76b
tatomyr <andrew.tatomyr@gmail.com>
2019-06-17 14:40:07 +0300

    Decouple InputForm

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,4 +1,5 @@
-export { TasksList } from './TasksList.js'
 export { NavBar } from './NavBar.js'
+export { TasksList } from './TasksList.js'
+export { InputForm } from './InputForm.js'
 export { FullscreenImage } from './FullscreenImage.js'
 export { Notification } from './Notification.js'
--- src/ui/pages/TodoPage.js
+++ src/ui/pages/TodoPage.js
@@ -1,37 +1,12 @@
 import {
-  NavBar, TasksList, FullscreenImage, Notification,
+  NavBar, TasksList, InputForm, FullscreenImage, Notification,
 } from '../components/index.js'
 
 export const TodoPage = () => `
   <div id="todo-page" class="container">
     ${NavBar()}
     ${TasksList()}
-    <div class="form">
-      <form
-        onSubmit="
-          event.preventDefault()
-          dispatch({ type: 'CREATE_TASK', event })
-        "
-      >
-        <input
-          class="input"
-          id="newTask"
-          name="newTask"
-          placeholder="New task..."
-          required
-          maxlength="60"
-          autocomplete="off"
-          onkeyup="dispatch({ type: 'CHANGE_INPUT', input: event.target.value })"
-        />
-        <div
-          id="clear"
-          class="round"
-          onclick="dispatch({ type: 'CLEAN_INPUT', target: 'newTask' })"
-        >
-          ✗
-        </div>
-      </form>
-    </div>
+    ${InputForm()}
     ${FullscreenImage()}
     ${Notification()}
   </div>
